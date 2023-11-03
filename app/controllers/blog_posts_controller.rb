6a9982e23365875911573b919fb7dcffb0b1e1fa
Yesim Dundar <kuranyesim@gmail.com>
2023-11-03 01:37:20 +0100

    enable to create a new blog post

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,13 @@ class BlogPostsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    
+  end
+
 end
