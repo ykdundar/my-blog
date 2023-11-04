c06ad9e43617d8bdef8e97ab895363974de21d21
Yesim Dundar <kuranyesim@gmail.com>
2023-11-04 23:44:13 +0100

    enable to update a new blog post

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts
   get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  # patch "/blog_posts/:id", to: "blog_posts#update"
+  patch "/blog_posts/:id", to: "blog_posts#update"
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
   # Defines the root path route ("/")
   root "blog_posts#index"
