df32b930811b6d485e0e6426d8bd0f0799ad4f2e
Yesim Dundar <kuranyesim@gmail.com>
2023-11-05 01:29:10 +0100

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  delete "/blog_posts/:id", to: "blog_posts#destroy"
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  # Defines the root path route ("/")
+  resources :blog_posts
   root "blog_posts#index"
 end
