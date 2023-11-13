e3676a3f6cca39044bbd0f25daee098f673fa746
Yesim Dundar <kuranyesim@gmail.com>
2023-11-13 03:20:38 +0100

    Enhence seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+BlogPost.destroy_all
 user =User.where(email: "yk@gmail.com").first_or_initialize
 user.update!(
    password: "password",
    password: "password"
    )
-puts "Creating restaurants..."
+puts "Creating posts..."
 
 BlogPost.create([
-{title: "Hello World", body:"This is my first blog post"},
-{title: "Hello Cosmos", body:"This is my second blog post"}
+{title: "Hello World", body:"This is my published blog post", published_at: DateTime.current },
+{title: "Hello Mars", body:"This is my scheduled blog post",  published_at: 1.day.from_now},
+{title: "Hello Cosmos", body:"This is my draft blog post"}
 ])
+puts BlogPost.all
 puts "Finished!"
