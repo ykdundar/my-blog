69b8ca443b25c6ee7aeb71867e9c1702a78edd51
Yesim Dundar <kuranyesim@gmail.com>
2023-11-14 02:16:25 +0100

    Fix bodies in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,13 @@ BlogPost.destroy_all
 user =User.where(email: "yk@gmail.com").first_or_initialize
 user.update!(
    password: "password",
-   password: "password"
    )
 puts "Creating posts..."
 
 BlogPost.create([
-{title: "Hello World", body:"This is my published blog post", published_at: DateTime.current },
-{title: "Hello Mars", body:"This is my scheduled blog post",  published_at: 1.day.from_now},
-{title: "Hello Cosmos", body:"This is my draft blog post"}
+{title: "Hello World", content:"This is my published blog post", published_at: DateTime.current},
+{title: "Hello Mars", content:"This is my scheduled blog post",  published_at: 1.day.from_now},
+{title: "Hello Cosmos", content:"This is my draft blog post"}
 ])
 puts BlogPost.all
 puts "Finished!"
