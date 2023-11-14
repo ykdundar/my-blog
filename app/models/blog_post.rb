588593ee62cfb835bbc97aa923767257e8a5a527
Yesim Dundar <kuranyesim@gmail.com>
2023-11-14 01:54:38 +0100

    Replace bodies all over the code to content

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,7 +1,7 @@
 class BlogPost < ApplicationRecord
   has_rich_text :content
   validates :title, presence: true
-  validates :body, presence: true
+  validates :content, presence: true
   scope :sorted, -> {order(arel_table[:published_at ].desc.nulls_last).order(updated_at: :desc)}
   scope :draft, ->{where(published_at: :nil)}
   scope :published, -> {where("published_at <= ?", Time.current)}
