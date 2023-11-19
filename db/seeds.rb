71b67b1468e50019df8d2d8e23d738fab5a06aac
Yesim Dundar <kuranyesim@gmail.com>
2023-11-19 01:01:34 +0100

    Add 100 blog posts to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
-# frozen_string_literal: true
+require 'date'
 
 BlogPost.destroy_all
 user = User.where(email: 'yk@gmail.com').first_or_initialize
 user.update!(
   password: 'password'
 )
+
 puts 'Creating posts...'
 
-BlogPost.create([
-                  { title: 'Hello World', content: 'This is my published blog post', published_at: DateTime.current },
-                  { title: 'Hello Mars', content: 'This is my scheduled blog post', published_at: 1.day.from_now },
-                  { title: 'Hello Cosmos', content: 'This is my draft blog post' }
-                ])
+time_examples = [DateTime.current, Date.today.next_year, nil ]
+100.times do |i|
+
+   BlogPost.create(title: "Blog Post #{i+1}", content: 'This is my published blog post', published_at: time_examples.sample)
+   puts i+1
+end
 puts BlogPost.all
 puts 'Finished!'
