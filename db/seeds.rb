dfe3eedf95879398ee3bb09dfb16f4f84b08eed9
Yesim Dundar <kuranyesim@gmail.com>
2023-11-20 02:39:38 +0100

    Hide pagy-nav if number of posts less than pagy items

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ user.update!(
 puts 'Creating posts...'
 
 time_examples = [DateTime.current, Date.today.next_year, nil]
-3.times do |i|
+100.times do |i|
   BlogPost.create(title: "Blog Post #{i + 1}", content: 'This is my published blog post',
                   published_at: time_examples.sample)
   puts i + 1
