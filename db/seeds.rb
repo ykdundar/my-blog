f46aafea4262c683d84eb5b5596b048edd2279ab
Yesim Dundar <kuranyesim@gmail.com>
2023-11-20 02:15:49 +0100

    Hide pagy-nav incase there is no page 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@ user.update!(
 
 puts 'Creating posts...'
 
-time_examples = [DateTime.current, Date.today.next_year, nil ]
-100.times do |i|
-
-   BlogPost.create(title: "Blog Post #{i+1}", content: 'This is my published blog post', published_at: time_examples.sample)
-   puts i+1
+time_examples = [DateTime.current, Date.today.next_year, nil]
+3.times do |i|
+  BlogPost.create(title: "Blog Post #{i + 1}", content: 'This is my published blog post',
+                  published_at: time_examples.sample)
+  puts i + 1
 end
 puts BlogPost.all
 puts 'Finished!'
