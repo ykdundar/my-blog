c643c042a8ad3b9fb35603040a05d22e8d3553d3
Yesim Dundar <kuranyesim@gmail.com>
2023-11-21 23:29:53 +0100

    Create cover image controller and destroy action

---
+++ app/controllers/blog_posts/cover_images_controller.rb
@@ -0,0 +1,15 @@
+class BlogPosts::CoverImages < ApplicationController
+  before_action :autenticate_user!
+  before_action :set_blog_post
+
+  def destroy
+    @blog_post.cover_image.purge_later
+    redirect_to edit_blog_post_path(@blog_post)
+  end
+
+  private
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:blog_post_id])
+  end
+end
