bfddd36761491d4bf90cb012e529f7e17a1d6c75
Yesim Dundar <kuranyesim@gmail.com>
2023-11-21 18:21:27 +0100

    Create a route for the feature of deleting cover post

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@
 Rails.application.routes.draw do
   devise_for :user, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+      def
+
+      end
+    end
+
+  end
+
   root 'blog_posts#index'
 end
